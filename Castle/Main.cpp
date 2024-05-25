e1b0bf7ded54d338a6eaa48bac08c924ff4e1587
ignasia_sundavr <theshadowhunterrr@gmail.com>
2024-05-25 15:04:57 +1200

    problem with reading position, probobly i just dont know what im doing.

---
+++ Castle/Main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+class Entity 
+{
+
+private:
+
+	float posX = 0;
+	float posY = 0;
+	float posZ = 0;
+
+public:
+
+	void Changepos(float x,float y,float z) {
+
+		posX = posX + x;
+		posY = posY + y;
+		posZ = posZ + z;
+	}
+
+	void Position() {
+
+		std::cout << "X = " << posX << std::endl;
+		std::cout << "Y = " << posY << std::endl;
+		std::cout << "Z = " << posZ << std::endl;
+
+	}
+
+};
+
+
+int main(){
+
+	Entity e;
+	e.Changepos(12, 14, 2);
+	e.Position;
+	std::cin.get();
+}
